360e8e381eb143183255aa908bac67ed85466892
Andrew Kane <acekane1@gmail.com>
2014-07-02 22:27:55 -0700

    Test nil

--- test/hightop_test.rb
+++ test/hightop_test.rb
@@ -28,6 +28,25 @@ class TestHightop < Minitest::Test
     assert_equal expected, Visit.limit(2).top(:city)
   end
 
+  def test_nil_values
+    create("San Francisco", 3)
+    create(nil, 2)
+    expected = {
+      "San Francisco" => 3,
+    }
+    assert_equal expected, Visit.top(:city)
+  end
+
+  def test_nil_option
+    create("San Francisco", 3)
+    create(nil, 2)
+    expected = {
+      "San Francisco" => 3,
+      nil => 2
+    }
+    assert_equal expected, Visit.top(:city, nil: true)
+  end
+
   def create(city, count = 1)
     count.times{ Visit.create!(city: city) }
   end
