14d37f45f77e707d4fa75a3bde0d582a023efea6
Andrew Kane <andrew@chartkick.com>
2019-06-07 12:18:19 -0700

    Cleaner code

--- hightop.gemspec
+++ hightop.gemspec
@@ -18,6 +18,8 @@ Gem::Specification.new do |spec|
 
   spec.required_ruby_version = ">= 2.3"
 
+  spec.add_dependency "activesupport", ">= 4.2"
+
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest"
--- lib/hightop.rb
+++ lib/hightop.rb
@@ -1,40 +1,9 @@
-require "hightop/version"
+# dependencies
 require "active_support"
 
-module Hightop
-  module Kicks
-    def top(column, limit = nil, distinct: nil, uniq: nil, min: nil, nil: nil)
-      distinct ||= uniq
-      order_str = column.is_a?(Array) ? column.map(&:to_s).join(", ") : column
-      relation = group(column).order(["count_#{distinct || 'all'} DESC", order_str])
-      if limit
-        relation = relation.limit(limit)
-      end
-
-      # terribly named option
-      unless binding.local_variable_get(:nil)
-        (column.is_a?(Array) ? column : [column]).each do |c|
-          relation = relation.where("#{c} IS NOT NULL")
-        end
-      end
-
-      if min
-        relation = relation.having("COUNT(#{distinct ? "DISTINCT #{distinct}" : '*'}) >= #{min.to_i}")
-      end
-
-      if distinct
-        # since relation.respond_to?(:distinct) can't be used
-        if ActiveRecord::VERSION::MAJOR > 3
-          relation.distinct.count(distinct)
-        else
-          relation.uniq.count(distinct)
-        end
-      else
-        relation.count
-      end
-    end
-  end
-end
+# modules
+require "hightop/kicks"
+require "hightop/version"
 
 ActiveSupport.on_load(:active_record) do
   extend Hightop::Kicks
