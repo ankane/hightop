2d00a53d3e8d801cf33a3f56e9294e1e395a9046
Andrew Kane <andrew@ankane.org>
2024-12-29 13:52:01 -0800

    Removed unneeded require [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
-require "logger"
 
 $logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
 
